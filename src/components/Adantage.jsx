1a984a5a3003d59755ac6799ef9994bfb256435f
unknown <bink@email.ua>
2023-08-21 23:38:43 +0300

    fix

--- src/components/Adantage.jsx
+++ src/components/Adantage.jsx
@@ -25,17 +25,6 @@ const Adantage = ({ title, id, toggle, activeId }) => {
 };
 
 const Wrapper = styled.section`
-  .advantage {
-    width: 350px;
-    height: 100%;
-    /* display: flex; */
-    padding: 22px 30px;
-    background-color: #f3efe8;
-    border-radius: 110px;
-    /* gap: 22px; */
-    align-items: center;
-    cursor: pointer;
-  }
   p {
     color: var(--5-d-2-b-6-f, #5d2b6f);
     font-family: Open Sans;
